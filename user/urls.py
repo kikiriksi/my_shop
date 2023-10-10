bae0df5c2796355ff0f278dde6af5f00b2a8caf0
Dmitriy <vertyos@mail.ru>
2023-10-10 13:26:00 +0300

    доделал вход в систему пользователей

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+
+app_name = 'user'
 urlpatterns = [
-    path('/login', views.Login.as_view(), name='login'),
+    path('login', views.login, name='login'),
 
 ]
--- user/views.py
+++ user/views.py
@@ -1,23 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import User
 from .forms import Login
-from django.views import View
 from django.contrib import auth
 
 
-class Login(View):
-    def get(self, request):
-        context = {'form_login': Login()}
-        return render(request, 'user/login.html', context)
-
-    def post(self, request):
+def login(request):
+    if request.method == 'POST':
         form = Login(data=request.POST)
         if form.is_valid():
             username = request.POST['username']
             password = request.POST['password']
             user = auth.authenticate(username=username, password=password)
             if user:
-                auth.login(user)
-        else:
-            context = {'form_login': Login()}
-            return render(request, 'user/login.html', context)
+                auth.login(request, user)
+                return redirect('home')
+    else:
+        context = {'form_login': Login()}
+        return render(request, 'user/login.html', context)
